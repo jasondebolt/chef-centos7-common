15dc85d35059835974b4b439743e29b610423871
jasondebolt <jasondebolt@gmail.com>
2017-04-18 16:12:07 -0700

    adding some dummy rspec tests

--- spec/unit/recipes/export-node_spec.rb
+++ spec/unit/recipes/export-node_spec.rb
@@ -6,15 +6,11 @@
 
 require 'spec_helper'
 
-describe 'chef-centos7-common::export-node' do
+describe 'chef-centos7-common::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
     end
   end
 end
